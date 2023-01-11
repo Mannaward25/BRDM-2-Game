d885476b0599cd1c9bea8887f223619142f1d7cb
Mannaward <88430213+Mannaward25@users.noreply.github.com>
2023-01-11 16:50:04 +0300

    Added new files

---
+++ BRMD.py
@@ -0,0 +1,16 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((500, 700))
+pygame.display.set_caption('BRDM-2 Game')
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.flip()
+    clock.tick(30)
+
+
