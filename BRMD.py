ac2bb56bc45e451ad2000792e69177d832adb5ec
Mannaward <88430213+Mannaward25@users.noreply.github.com>
2023-01-11 23:02:54 +0300

    Added new files

--- BRMD.py
+++ BRMD.py
@@ -1,19 +1,43 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((500, 700))
+
+W = 600
+H = 400
+
+screen = pygame.display.set_mode((W, H))
 pygame.display.set_caption('BRDM-2 Game')
 pygame.display.set_icon(pygame.image.load("BRDM-2.1.bmp"))
 clock = pygame.time.Clock()
+FPS = 60
 running = True
 
-pygame.draw.rect(screen,(255,255,255), (10,10, 50, 100), 2)
-pygame.display.update()
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+
+# Полезности
+# pygame.draw.rect(screen, WHITE, (10,10, 50, 100), 2)
+# pygame.draw.line(screen, BLUE, (10, 200), (350, 50))
+# pygame.draw.aaline(screen, GREEN, (200, 40), (350, 50))
+# pygame.draw.polygon(screen,GREEN, [[150, 210], [180,250],[90, 290],[30,230]], 1)
+# pygame.display.update()
+
+x = W // 2
+y = H // 2
+speed = 5
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    pygame.display.flip()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x -= speed
+            elif event.key == pygame.K_RIGHT:
+                x += speed
+    screen.fill(WHITE)
+    pygame.draw.rect(screen, GREEN, (x, y, 10, 20))
+    pygame.display.update()
     clock.tick(60)
 
