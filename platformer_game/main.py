f7ad5f468e864493c9b2335ca923ce1c435721a0
Paul <ppolushkin.93@mail.ru>
2023-02-26 19:06:25 +0300

    new game

--- platformer_game/main.py
+++ platformer_game/main.py
@@ -4,20 +4,20 @@ from settings import *
 class Game:
     def __init__(self):
         pass
-
+    
     def game_init(self):
         pass
-
+    
     def update(self):
         pass
-
+    
     def draw(self):
         pass
-
+        
     def run(self):
         pass
-
-
+    
+    
 if __name__ == '__main__':
     app = Game()
     app.run()
