d85a3be3c60b35a413c1727daef83454fca7be09
ppolu <ppolushkin.93@mail.ru>
2023-03-21 02:01:14 +0300

    menu experiments

--- menus_trial/main.py
+++ menus_trial/main.py
@@ -155,46 +155,158 @@ class MultiplayerMenu(Menu):
         self.back_btn.update()
 
 
+class QuitMenu(Menu):
+
+    def __init__(self, app, visible=True):
+        super().__init__(app, visible)
+        self.yes_btn: Button = Button(self, 'Yes', pos=(100, 100), btn_id=7)
+        self.no_btn: Button = Button(self, 'No', pos=(100, 150), btn_id=8)
+
+        self.btn_list = self.get_buttons()
+
+    def get_buttons(self):
+        return [self.yes_btn, self.no_btn]
+
+    def update(self):
+        super().update()
+        self.yes_btn.update()
+        self.no_btn.update()
+
+
+class GamePause(Menu):
+
+    def __init__(self, app, visible=True):
+        super().__init__(app, visible)
+        self.continue_btn: Button = Button(self, 'Continue', pos=(100, 100), btn_id=9)
+        self.quit_btn: Button = Button(self, 'Quit Game', pos=(100, 150), btn_id=4)
+
+        self.btn_list = self.get_buttons()
+
+    def get_buttons(self):
+        return [self.continue_btn, self.quit_btn]
+
+    def update(self):
+        super().update()
+        self.continue_btn.update()
+        self.quit_btn.update()
+
+
+class ContextNode:
+
+    def __init__(self, menu, node=None):
+        self.data = menu
+        self.prev = node
+
+
+class MenuHistory:
+
+    def __init__(self):
+        self.head = None
+        self.cur = None
+
+    def add(self, data):
+        if not self.head:
+            self.head = ContextNode(data)
+            self.cur = self.head
+        else:
+            prev = self.cur
+            self.cur = ContextNode(data, prev)
+
+    def get_prev(self):
+        return self.cur.prev.data
+
+    def show_all(self):
+        node = self.cur
+        count = 0
+        while node:
+            print(count, node.data)
+            count += 1
+            node = node.prev
+
+    def clear(self):
+        self.head = self.cur
+        self.cur = None
+
+
 class MenuRenderer:
 
     def __init__(self, app):
         self.app = app
+        self.screen = app.screen
+
         self.menu = MainMenu(self.app)
         self.multiplayer_menu = MultiplayerMenu(self.app, visible=DISABLE)
+        self.quit_menu = QuitMenu(self.app, visible=DISABLE)
+        self.game_pause = GamePause(self.app, visible=DISABLE)
+
+        self.prev_menu_to_render = None
+        self.node = MenuHistory()
         self.menu_to_render = None
 
         self.init_menu()
 
         self.switch_menu = {
-            0: False,
-            1: self.menu,
+            0: self.menu_to_render,
+            1: self.start_game,
             2: self.multiplayer_menu,
             3: self.menu,
-            4: self.quit,
+            4: self.quit_menu,
             5: self.multiplayer_menu,
-            6: self.menu
+            6: self.menu,
+            7: self.quit,
+            8: self.back_to_prev_menu,
+            9: self.start_game
         }
 
     def choose_menu(self):
-        self.menu_to_render.check_event()
-        opt = self.menu_to_render.get_option()
-        self.menu_to_render.set_visible(DISABLE)
-        self.menu_to_render = self.switch_menu[opt]
+
+        if self.menu_to_render and not callable(self.menu_to_render):
+            self.menu_to_render.check_event()
+            opt = self.menu_to_render.get_option()
+            self.menu_to_render.set_visible(DISABLE)
+
+            self.menu_to_render = self.switch_menu[opt]
+
+            if not callable(self.menu_to_render):
+                self.node.add(self.menu_to_render)
+
+            self.switch_menu.update({0: self.menu_to_render})
+
         if self.menu_to_render and not callable(self.menu_to_render):
             self.menu_to_render.set_visible(ENABLE)
+
         elif self.menu_to_render and callable(self.menu_to_render):
             self.menu_to_render()
 
     def init_menu(self):
         self.menu_to_render = self.menu
+        self.node.add(self.menu_to_render)
 
     def render(self):
-        if self.menu_to_render:
-            self.menu_to_render.update()
+        if self.app.menu_flag:
+
+            if callable(self.menu_to_render) and self.app.game_start_flag:
+                self.menu_to_render = self.game_pause
+                self.menu_to_render.set_visible(ENABLE)
+                self.node.add(self.menu_to_render)
+
+            if self.menu_to_render and self.menu_to_render.visible:
+                self.menu_to_render.update()
+
+    def back_to_prev_menu(self):
+        self.node.show_all()
+        self.menu_to_render = self.node.get_prev()
+        self.menu_to_render.set_visible(ENABLE)
+        self.node.add(self.menu_to_render)
 
     def quit(self):
         pg.quit()
         sys.exit()
+
+    def start_game(self):
+        self.app.menu_flag = False
+        self.app.game_start_flag = True
+        self.node.clear()
 
 
 class App:
@@ -205,32 +317,47 @@ class App:
         self.clock = pg.time.Clock()
         self.delta_time = 1
 
+        self.menu_flag = True
+        self.game_start_flag = False
+
         self.menu_render = MenuRenderer(self)
 
     def draw(self):
-        self.screen.fill('dark green')
+        self.screen.fill('black')
 
     def check_events(self):
         for event in pg.event.get():
-            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+            if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
-            if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+            elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE and not self.menu_flag:
+                self.menu_flag = True
+            elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE and self.menu_flag and self.game_start_flag:
+                self.menu_flag = False
+
+            if event.type == pg.MOUSEBUTTONDOWN and event.button == 1 and self.menu_flag:
                 self.menu_render.choose_menu()
             if event.type == pg.MOUSEBUTTONUP and event.button == 1:
                 pass
 
     def update(self):
-        self.menu_render.render()
 
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'menu app')
 
+    def draw_menu(self):
+        self.screen.fill('dark green')
+        self.menu_render.render()
+
     def run(self):
         while True:
             self.check_events()
-            self.draw()
+            if self.menu_flag:
+                self.draw_menu()
+            else:
+                self.draw()
+
             self.update()
 
 
